b7b884142fc8b2610ed5d8503e1fd9062c8c456c
Janosch <janosch@janosch.dev>
2023-02-23 08:47:49 +0100

    Use mysql instead of sqlite

--- prisma/migrations/20221104163325_sessions_added/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Session" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "expiresAt" DATETIME NOT NULL,
-    "token" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
-    CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
